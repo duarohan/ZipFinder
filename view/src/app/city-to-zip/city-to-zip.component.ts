45a08dc7001031a48f5d88955077807b86024481
Rohan Dua <rohan.dua@nihilent.com>
2019-01-06 09:12:07 +0200

    Show State/City/Zip Attributes when required

--- view/src/app/city-to-zip/city-to-zip.component.ts
+++ view/src/app/city-to-zip/city-to-zip.component.ts
@@ -10,27 +10,39 @@ import {CityToZipService} from '../city-to-zip.service';
   providers :[CityToZipService]
 })
 export class CityToZipComponent implements OnInit {
-
-
-  constructor(private cityToZipService:CityToZipService) { }
   countryCollection = []
   stateCollection = []
   cityCollection = []
   zipCodeVal;
+  HideStateDropDown;
+  HideCityDropDown;
+  HideZipLabel;
+
+  constructor(private cityToZipService:CityToZipService) { }
+  
   ngOnInit() {
     this.cityToZipService.getCountryService().subscribe(data=> this.countryCollection = data);
+    this.HideStateDropDown = true;
+    this.HideCityDropDown = true;
+    this.HideZipLabel = true;
   }
 
   getState(country){
     this.cityToZipService.getStateService(country).subscribe(data=> this.stateCollection = data);
+    this.HideStateDropDown = false;
+    this.HideCityDropDown = true;
+    this.HideZipLabel = true;
   }
 
   getCity(state,country){
     this.cityToZipService.getCityService(state,country).subscribe(data=> this.cityCollection = data);
+    this.HideCityDropDown = false;
+    this.HideZipLabel = true;
   } 
 
   getZip(city,state,country){
     this.cityToZipService.getZipService(city,state,country).subscribe(data=> this.zipCodeVal = data);
+    this.HideZipLabel = false;
   } 
 
 }
